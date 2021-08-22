2cf16f008ee6f5d3b2772b3c45a4dbc8616a7aad
Luiz Rioja <lulyrioja@gmail.com>
2021-08-21 21:03:21 -0300

    :alien: Inserting keys in DynamoDB connection options

--- src/utils/dynamodbClient.ts
+++ src/utils/dynamodbClient.ts
@@ -3,6 +3,8 @@ import { DynamoDB } from "aws-sdk";
 const options = {
   region: "localhost",
   endpoint: "http://localhost:8000",
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 };
 
 const isOffline = () => {
